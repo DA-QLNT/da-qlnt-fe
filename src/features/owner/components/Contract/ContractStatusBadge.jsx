2964ea27127755312b902c74dfdfb9eadd8425a7
HoaHoa <itlmh2k3@gmail.com>
2025-12-19 06:05:33 +0700

    init i18n for contract

--- src/features/owner/components/Contract/ContractStatusBadge.jsx
+++ src/features/owner/components/Contract/ContractStatusBadge.jsx
@@ -1,14 +1,16 @@
 import { CONTRACT_STATUS_MAP } from "@/assets/contract/contractStatus";
 import { Badge } from "@/components/ui/badge";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const ContractStatusBadge = ({ contractStatus }) => {
+  const { t } = useTranslation("contractinvoice");
   const statusInfo =
     CONTRACT_STATUS_MAP[contractStatus] || CONTRACT_STATUS_MAP[0];
   const { label, color } = statusInfo;
   return (
     <Badge className={`uppercase text-muted-foreground ${color}`}>
-      {label}
+      {t(`${label}`)}
     </Badge>
   );
 };
